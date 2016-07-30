ce2ff7e71e124e79a5839e3a46c9a81bca492cd7
Alessandro Tundo <alessandrotundo94@gmail.com>
2016-07-30 19:18:49 +0200

    Added missing files into previous commit
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

---
+++ modules/ve-easylink/ve.ui.easyLinkEditDialog2.js
@@ -0,0 +1,143 @@
+/*!
+ * VisualEditor UserInterface easyLinkEditDialog2 class.
+ *
+ * @copyright 2011-2016 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Inspector for inserting special characters.
+ *
+ * @class
+ * @extends ve.ui.ToolbarDialog
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.easyLinkEditDialog2 = function VeUieasyLinkEditDialog2(fragment, annotation, config) {
+	// Parent constructor
+	ve.ui.easyLinkEditDialog2.super.apply( this, config );
+  this.pages = null;
+  this.annotation = annotation;
+  this.fragment = fragment;
+	this.$element.addClass( 've-ui-easyLinkEditDialog2' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ui.easyLinkEditDialog2, OO.ui.ProcessDialog );
+
+/* Static properties */
+
+ve.ui.easyLinkEditDialog2.static.name = 'easyLinkEditDialog2';
+ve.ui.easyLinkEditDialog2.static.size = 'large';
+ve.ui.easyLinkEditDialog2.static.title = 'Edit';
+ve.ui.easyLinkEditDialog2.static.actions = [{
+  'action': 'save',
+  'label': 'Done',
+  'flags': ['primary', 'constructive'],
+  'modes': 'intro',
+  'icon': 'check'
+}, {
+  'label': OO.ui.deferMsg('visualeditor-dialog-action-cancel'),
+  'flags': 'safe',
+  'modes': 'intro',
+  'icon': 'close'
+}];
+
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+ve.ui.easyLinkEditDialog2.prototype.initialize = function () {
+	// Parent method
+	ve.ui.easyLinkEditDialog2.super.prototype.initialize.call( this );
+};
+
+/**
+ * @inheritdoc
+ */
+ve.ui.easyLinkEditDialog2.prototype.getReadyProcess = function ( data ) {
+	data = data || {};
+	return ve.ui.easyLinkEditDialog2.super.prototype.getReadyProcess.call( this, data )
+		.next( function () {
+			if ( !this.pages ) {
+        this.buildPages();
+			}
+		}, this );
+};
+
+/* Set the body height */
+ve.ui.easyLinkEditDialog2.prototype.getBodyHeight = function() {
+  return 300;
+};
+
+/* Define actions */
+ve.ui.easyLinkEditDialog2.prototype.getActionProcess = function(action) {
+  var dialogEdit = this;
+  if (action === 'save') {
+    return new OO.ui.Process(function() {
+      var newGloss = dialogEdit.bookletLayout.getPage('glosses').$glossText.text();
+      var newGlosses = [];
+      $.each(dialogEdit.annotation.getGlosses(), function(key, value){
+        newGlosses.push(value);
+      })
+      var newWikiLink = null;
+      if(dialogEdit.bookletLayout.getPage('wikiLink')){
+        newWikiLink = dialogEdit.bookletLayout.getPage('wikiLink').inputWidget.getValue();
+      }
+      var newGlossSource = dialogEdit.bookletLayout.getPage('glosses').dropdownWidget.getMenu().getSelectedItem().getLabel();
+      if(!dialogEdit.bookletLayout.getPage('glosses').dropdownWidget.getMenu().getItemFromLabel(newGlossSource) || newGlossSource === 'WikiToLearn'){
+        newGlosses.push({gloss: newGloss, glossSource: newGlossSource});
+      }
+      var newAnnotation = new ve.dm.easyLinkAnnotation({
+        type: 'link/easyLink',
+        attributes: {
+          babelnetId: dialogEdit.annotation.getBabelnetId(),
+          title: dialogEdit.annotation.getTitle(),
+          gloss: newGloss,
+          glossSource: newGlossSource,
+          glosses: newGlosses,
+          babelLink: dialogEdit.annotation.getBabelLink(),
+          wikiLink: newWikiLink
+        }
+      });
+      dialogEdit.fragment.annotateContent('clear', dialogEdit.annotation);
+      dialogEdit.fragment.annotateContent('set', newAnnotation);
+      dialogEdit.close( { action: action } );
+  }, this);
+  }
+return ve.ui.easyLinkEditDialog2.super.prototype.getActionProcess.call(this, action);
+};
+
+/**
+ * Builds the pages
+ */
+ve.ui.easyLinkEditDialog2.prototype.buildPages = function () {
+
+	this.bookletLayout = new OO.ui.BookletLayout( {
+		outlined: true,
+		continuous: false
+	} );
+	this.pages = [];
+  var annotationKeys = Object.keys(this.annotation.getAttributes());
+  for(i=0; i<annotationKeys.length; i++){
+    if(annotationKeys[i] === 'glosses' || annotationKeys[i] === 'wikiLink'){
+      this.pages.push(
+  			new ve.ui.easyLinkPage( annotationKeys[i], {
+  				label: annotationKeys[i],
+  				attribute: this.annotation.getAttribute(annotationKeys[i])
+  			} )
+  		);
+    }
+  }
+	this.bookletLayout.addPages( this.pages );
+	this.$body.append( this.bookletLayout.$element );
+
+	this.updateSize();
+};
+
+/* Registration */
+
+ve.ui.windowFactory.register( ve.ui.easyLinkEditDialog2 );
---
+++ modules/ve-easylink/ve.ui.easyLinkPage.js
@@ -0,0 +1,136 @@
+/**
+ * MediaWiki meta dialog Languages page.
+ *
+ * @class
+ * @extends OO.ui.PageLayout
+ *
+ * @constructor
+ * @param {string} name Unique symbolic name of page
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.easyLinkPage = function ( name, config ) {
+
+	// Parent constructor
+	OO.ui.PageLayout.call( this, name, config );
+
+	this.label = config.label;
+	this.attribute = config.attribute;
+  this.inputWidget = null;
+  if(this.label === 'wikiLink'){
+    this.inputWidget = this.createInputWidget();
+    this.inputWidget.setValue(this.attribute);
+    this.$textDiv = $( '<div>' ).append(this.inputWidget.$element);
+  }else if(this.label === 'glosses'){
+    this.inputWidget = this.createTextareaWidget();
+    //this.inputWidget.setValue(this.attribute[0].gloss);
+    this.$glossText = $('<p>').html(this.attribute[0].gloss);
+    this.$textDiv = $( '<div>' ).append(this.$glossText);
+    this.dropdownWidget = this.createDropdown();
+    this.dropdownWidget.getMenu().selectItemByLabel(this.attribute[0].glossSource);
+    this.fieldLayout = new OO.ui.FieldLayout(this.dropdownWidget, {
+      label: 'Source: ',
+      align: 'top'
+    } );
+    this.addButton = new OO.ui.ButtonWidget( {
+      label: 'Add a WikiToLearn gloss',
+      icon: 'add',
+      iconTitle: 'Add'
+    } );
+    this.confirmButton = new OO.ui.ButtonWidget( {
+      label: 'Confirm',
+      icon: 'check',
+      iconTitle: 'Confirm'
+    } );
+    this.$textDiv.append(this.$textDiv.$element, this.inputWidget.$element, this.fieldLayout.$element, this.addButton.$element, this.confirmButton.$element);
+    this.inputWidget.toggle();
+    this.confirmButton.toggle();
+    this.addButton.connect(this, {'click': 'onAddButtonClick'});
+    this.confirmButton.connect(this, {'click': 'onConfirmButtonClick'});
+    this.dropdownWidget.connect(this, {'labelChange': 'onLabelChange'});
+  }
+	this.$element
+		.addClass( 've-ui-easyLinkPage' )
+		.append( $( '<h3>' ).text( name ), this.$textDiv );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ui.easyLinkPage, OO.ui.PageLayout );
+
+/* Methods */
+
+ve.ui.easyLinkPage.prototype.onLabelChange = function(){
+  var gloss = this.dropdownWidget.getMenu().getSelectedItem().getData();
+  this.$glossText.html(gloss);
+};
+
+ve.ui.easyLinkPage.prototype.onAddButtonClick = function(){
+  this.$glossText.hide();
+  this.inputWidget.toggle();
+  this.fieldLayout.toggle();
+  this.addButton.toggle();
+  this.confirmButton.toggle();
+};
+
+ve.ui.easyLinkPage.prototype.onConfirmButtonClick = function(){
+  var newGloss = this.inputWidget.getValue();
+  this.inputWidget.toggle();
+  this.$glossText.html(newGloss);
+  this.$glossText.show();
+  this.confirmButton.toggle();
+  this.addButton.toggle();
+  if(!this.dropdownWidget.getMenu().getItemFromLabel('WikiToLearn')){
+    this.dropdownWidget.getMenu().addItems([new OO.ui.MenuOptionWidget( {
+        data: newGloss,
+        label: 'WikiToLearn',
+    } )]);
+  }else{
+    this.dropdownWidget.getMenu().getItemFromLabel('WikiToLearn').setData(newGloss);
+  }
+  this.dropdownWidget.getMenu().selectItemByLabel('WikiToLearn');
+  this.fieldLayout.toggle();
+};
+
+ve.ui.easyLinkPage.prototype.createDropdown = function(config) {
+  var items = [];
+  $.each(this.attribute, function(key, value){
+    items.push(new OO.ui.MenuOptionWidget( {
+        data: value.gloss,
+        label: value.glossSource,
+    } ));
+  });
+  return new OO.ui.DropdownWidget({
+    label: 'Seleziona una fonte',
+    menu: {
+      items: items
+    }
+  } );
+};
+
+/**
+ * Create a inputext to be used by the edit dialog widget
+ *
+ * @param {Object} [config] Configuration options
+ * @return {OO.ui.Widget} Text input widget
+ */
+ve.ui.easyLinkPage.prototype.createInputWidget = function ( config ) {
+	return new OO.ui.TextInputWidget( $.extend( { validate: 'non-empty' }, config ) );
+};
+
+/**
+ * Create a textarea to be used by the edit dialog widget
+ *
+ * @param {Object} [config] Configuration options
+ * @return {OO.ui.Widget} Text input widget
+ */
+ve.ui.easyLinkPage.prototype.createTextareaWidget = function ( config ) {
+	return new OO.ui.TextInputWidget( $.extend( { validate: 'non-empty', multiline: true, rows: 5 }, config ) );
+};
+
+/**
+ * @inheritdoc
+ */
+ve.ui.easyLinkPage.prototype.setupOutlineItem = function ( outlineItem ) {
+	ve.ui.easyLinkPage.super.prototype.setupOutlineItem.call( this, outlineItem );
+	this.outlineItem.setLabel( this.label );
+};
